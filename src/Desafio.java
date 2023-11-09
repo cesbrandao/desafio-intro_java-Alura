318f3b6e4f2e67f15855fd3f0082abdbfc9d32af
cesbrandao <ceduardosbrandao@gmail.com>
2023-11-08 23:10:09 -0300

    Inicialização dos dados do cliente

---
+++ src/Desafio.java
@@ -0,0 +1,14 @@
+public class Desafio {
+    public static void main(String[] args) {
+
+        String nome = "Carlos Eduardo Brandão";
+        String tipoConta = "Corrente";
+        double saldo = 1000.00;
+
+        System.out.println("========================================");
+        System.out.println("Nome do cliente: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("========================================");
+    }
+}
